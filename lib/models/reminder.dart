fac66fead0cf275a0847d678f2c8c2d13c127d4f
rahulshenoy1 <rahul.shenoy@exathought.com>
2024-05-21 20:00:31 +0530

    added the music and the list

--- lib/models/reminder.dart
+++ lib/models/reminder.dart
@@ -2,6 +2,9 @@ class Reminder {
   String dayOfWeek;
   String activity;
   DateTime time;
-  Reminder(
-      {required this.dayOfWeek, required this.activity, required this.time});
+  Reminder(this.dayOfWeek, this.activity, this.time);
+
+  String get formattedTime {
+    return '${time.hour}:${time.minute.toString().padLeft(2, '0')}';
+  }
 }
